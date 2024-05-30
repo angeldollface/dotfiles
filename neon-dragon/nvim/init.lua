a1cee2a3aaa9dd306a76f5c2df7e66297a6a412e
angeldollface <officialrealalexanderabraham@gmail.com>
2024-05-30 13:48:12 +0200

    Added my Neovim config.

--- neon-dragon/nvim/init.lua
+++ neon-dragon/nvim/init.lua
@@ -1,1 +1,13 @@
+-- Line numbers.
 vim.wo.number = true
+
+-- Transparent background.
+vim.cmd([[highlight Normal guibg=none]])
+vim.cmd([[highlight NormText guibg=none]])
+vim.cmd([[highlight Normal ctermbg=none]])
+vim.cmd([[highlight NonText ctermbg=none]])
+vim.cmd([[syntax on]])
+
+-- Shift and tab widths.
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
